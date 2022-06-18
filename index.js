9a1a3f2ca1a2658b56ce6aedd96b2ee9b69d7961
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-06-18 21:54:01 +0530

    Insert

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 8000;
+const cors=require('cors');
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.get('/dub',(req,res)=>{
+    const data=require('./src/data.json');
+    var min=1;
+    var max=Object.keys(data).length;
+    var diff=max-min;
+    var rand=Math.random();
+    rand=Math.floor(rand*diff);
+    rand=rand+min;
+    var x=""+rand;
+    var link=data[x];
+    res.send(
+        {
+            "img":link
+        }
+    )
+});
+
+app.listen(port);
